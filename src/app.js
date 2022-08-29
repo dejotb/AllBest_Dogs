9110a6c2e8e0ef48d48815434e58ddc42795b226
dejotb <dejotb@gmail.com>
2022-08-29 12:44:48 +0200

    added rendering of dog function

--- src/app.js
+++ src/app.js
@@ -2,26 +2,75 @@ require('dotenv').config();
 
 const URL = 'https://api.thedogapi.com/v1';
 
-async function getData() {
+const state = {
+  dog: {},
+  imageUrl: {},
+};
+
+async function getData(value) {
   try {
     if (!process.env.DOGS_API_KEY) {
       throw new Error('You forgot to set DOGS_API_KEY ');
     }
-    const data = await fetch(`${URL}/images/S1T8Ee9Nm`, {
+    const data = await fetch(`${URL}/breeds/search?q=${value}`, {
       headers: {
         'X-Api-Key': process.env.DOGS_API_KEY,
       },
     });
     const result = await data.json();
-    console.log(result);
-    const markup = `
-    <img src='${result.url}' alt=''>
-`;
 
-    document.querySelector('.image').insertAdjacentHTML('afterbegin', markup);
+    const dog = result[0];
+
+    state.dog = {
+      name: dog.name,
+      bred_for: dog.bred_for,
+      breed_group: dog.breed_group,
+      life_span: dog.life_span,
+      imgID: dog.reference_image_id,
+      temperament: dog.temperament,
+    };
   } catch (err) {
     console.log(err);
   }
 }
 
-getData();
+async function getImage(imageID) {
+  try {
+    if (!process.env.DOGS_API_KEY) {
+      throw new Error('You forgot to set DOGS_API_KEY ');
+    }
+    const data = await fetch(`${URL}/images/${imageID}`, {
+      headers: {
+        'X-Api-Key': process.env.DOGS_API_KEY,
+      },
+    });
+    const result = await data.json();
+    state.imageUrl = result.url;
+
+    console.log(state.imageUrl);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function showDog(breed) {
+  await getData(breed);
+  const { dog } = state;
+  await getImage(dog.imgID);
+  const { imageUrl } = state;
+  console.log(dog);
+
+  const markup = `
+  <p>Name: ${dog.name}</p>
+  <p>life span: ${dog.life_span}</p>
+  <p>${dog.temperament}</p>
+`;
+  const image = `
+  <img src='${imageUrl}' alt=''>
+  `;
+
+  document.querySelector('.image').insertAdjacentHTML('afterbegin', markup);
+  document.querySelector('.image').insertAdjacentHTML('afterbegin', image);
+}
+
+showDog('bor');
